3f4d40ab0fccae588113a084d84d9098d8f19526
Jonas Greitemann <jgreitemann@gmail.com>
2024-04-13 00:32:02 +0200

    Pretty up the help messages with some color

--- src/timetracker/cli.py
+++ src/timetracker/cli.py
@@ -2,6 +2,7 @@ import sys
 from pathlib import Path
 
 import click
+from click_help_colors import HelpColorsGroup
 
 from timetracker.worklog.transaction import transact
 
@@ -20,13 +21,19 @@ def cli_with_error_reporting():
         sys.exit(1)
 
 
-@click.group()
+@click.group(
+    cls=HelpColorsGroup,
+    help_headers_color="yellow",
+    help_options_color="green",
+    context_settings={"help_option_names": ["-h", "--help"]},
+)
 def cli():
     pass
 
 
 @cli.command()
 def status():
+    """Print a summary of the worklog by activity."""
     with transact(Path("~/Desktop/worklog.json").expanduser()) as worklog:
         click.echo(worklog)
 
@@ -34,6 +41,7 @@ def status():
 @cli.command()
 @click.argument("activity")
 def start(activity: str):
+    """Start a new activity or resume an existing one."""
     with transact(Path("~/Desktop/worklog.json").expanduser()) as worklog:
         worklog.update_activity(activity, lambda a: a.started())
 
@@ -41,5 +49,6 @@ def start(activity: str):
 @cli.command()
 @click.argument("activity")
 def stop(activity: str):
+    """Stop a running activity."""
     with transact(Path("~/Desktop/worklog.json").expanduser()) as worklog:
         worklog.update_activity(activity, lambda a: a.stopped())
